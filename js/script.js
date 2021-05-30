37ed9e87e2be0c529f343b7b2a418c5655a3a853
Roman <pogrebniakroman@gmail.com>
2021-05-30 20:14:16 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// "use strict";
+
+
+// console.log('arr' + "- object");
+
+let incr = 10;
+let decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+
+// console.log(2*4 == 8);
+
+const isChecked = true,
+isClose = true;
+
+console.log(isChecked && isClose);
