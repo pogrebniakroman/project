f80aadf8c9c4aca410d690b3995e91cfb2c99f19
Roman <pogrebniakroman@gmail.com>
2021-05-30 22:27:03 +0300

    fifth changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,56 @@
 
-const numberOfFilms = +prompt('How many films you watch?', '');
+// const numberOfFilms = +prompt('How many films you watch?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('One is last watched film', ''),
-      b = prompt('How you value film?', ''),
-      c = prompt('One is last watched film', ''),
-      d = prompt('How you value film?', '');
+// const a = prompt('One is last watched film', ''),
+//       b = prompt('How you value film?', ''),
+//       c = prompt('One is last watched film', ''),
+//       d = prompt('How you value film?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+  "use strict";
+
+ if(4==4){
+     console.log('Ok');
+ } else{
+     console.log('Error');
+ }
+ const num = 50;
+
+ if(num < 49){
+     console.log('Error');
+ }else if (num > 100){
+     console.log('Many');
+ } else{
+     console.log('Ok!');
+ }
+
+ (num == 50) ? console.log('Ok!') : console.log('Error');
+
+ switch(num){
+     case 49:
+         console.log('Incorrect');
+         break;
+
+         case 100:
+         console.log('Incorrect');
+         break;
+
+         case 50:
+         console.log('Correct');
+         break;
+         default:
+             console.log('All values incorrect');
+             break;
+ }
